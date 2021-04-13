28a22f9081e6edf16f9d695c5b97a51277562bea
Pablo Alonso <pepoalonso1@gmail.com>
2021-04-13 17:26:34 -0300

    se agrego el componente spinner con su logica

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
-import Frase from './components/Frase';
-import {useState, useEffect} from 'react';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
+import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
-   //crear el state
-   const [personaje, setPersonaje] = useState({});
+  //crear el state
+  const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     // aqui traigo los datos de la api
     consultarAPI();
-  },[]);
+  }, []);
 
-const consultarAPI = async() =>{
-  const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-  const resultado = await respuesta.json();
-  console.log(resultado[0]);
-  setPersonaje(resultado[0]);
-}
+  const consultarAPI = async () => {
+    // mostrar spinner
+    setCargando(true);
+    const respuesta = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
+    const resultado = await respuesta.json();
+    console.log(resultado[0]);
+    // mostrar el componente frase
+    setTimeout(() => {
+      setCargando(false);
+      setPersonaje(resultado[0]);
+    },1500);
+  };
+
+  // operador ternario
+  // (condicion logica)?(codigo cuando es verdadera):(codigo cuando la condicion es falsa)
+  const mostrarComponente =
+    cargando === true ? (
+      <Spinner></Spinner>
+    ) : (
+      <Frase sujeto={personaje}></Frase>
+    );
 
   return (
-    <section className='container d-flex flex-column my-5 align-items-center'>
-     <img src={process.env.PUBLIC_URL+'logo.png'} alt="logo de los simpsons" className='w-100'/>
-     <Button variant="warning" className='my-4 w-50' onClick={() => consultarAPI()}>Obtener frase</Button>
-     <Frase sujeto={personaje}></Frase>
+    <section className="container d-flex flex-column my-5 align-items-center">
+      <img
+        src={process.env.PUBLIC_URL + "logo.png"}
+        alt="logo de los simpsons"
+        className="w-100"
+      />
+      <Button
+        variant="warning"
+        className="my-4 w-50"
+        onClick={() => consultarAPI()}
+      >
+        Obtener frase
+      </Button>
+      {mostrarComponente}
     </section>
   );
 }
